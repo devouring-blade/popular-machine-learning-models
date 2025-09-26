6ff0d3afeb23d4991cd5d69b4684d096dc61bc28
Kiên Huy <163492408+devouring-blade@users.noreply.github.com>
2025-09-26 16:35:29 +0700

    Update knn.py

--- k nearest neighboors/knn.py
+++ k nearest neighboors/knn.py
@@ -2,7 +2,7 @@ import numpy as np
 from collections import Counter
 
 
-class Knn_classification:
+class KNN_Classification:
     def __init__(self, k= 3):
         self.k = k
 
@@ -11,10 +11,14 @@ class Knn_classification:
         self.y_train = y_train
 
     def predict(self, x_test):
+        def euclidean_distance(x, y):
+            distance = np.sqrt(np.sum((x - y) ** 2))
+            return distance
+
         y_predict = []
         for i in x_test:
             # computer the distance
-            distances = [self.euclidean_distance(i, j) for j in self.x_train]
+            distances = [euclidean_distance(i, j) for j in self.x_train]
 
             # get the closest k
             k_indices = np.argsort(distances)[: self.k]
@@ -26,9 +30,34 @@ class Knn_classification:
             y_predict.append(most_label)
         return y_predict
 
-    def euclidean_distance(self, x, y):
-        distance = np.sqrt(np.sum((x-y)**2))
-        return distance
+
+class KNN_Regression:
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, x_train, y_train):
+        self.x_train = x_train
+        self.y_train = y_train
+
+    def predict(self, x_test):
+        def euclidean_distance(x, y):
+            distance = np.sqrt(np.sum((x - y) ** 2))
+            return distance
+
+        y_predict = []
+        for i in x_test:
+            # computer the distance
+            distances = [euclidean_distance(i, j) for j in self.x_train]
+
+            # get the closest k
+            k_indices = np.argsort(distances)[: self.k]
+            k_nearest_values = [self.y_train[j] for j in k_indices]
+
+            # mean target k nearest
+            mean_k_nearest = np.mean(k_nearest_values)
+            y_predict.append(mean_k_nearest)
+        return y_predict
+
 
 
 
