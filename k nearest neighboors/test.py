8315cbcfb1f82adb3e63e1e8aa54e36bdfd0ac9c
Kiên Huy <163492408+devouring-blade@users.noreply.github.com>
2025-09-26 17:10:40 +0700

    Create test.py

---
+++ k nearest neighboors/test.py
@@ -0,0 +1,60 @@
+from sklearn.datasets import load_iris, load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from knn import KNN_Classification
+from sklearn.metrics import classification_report, r2_score, mean_squared_error, mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+
+
+def test_classification():
+    iris = load_iris(as_frame=True)
+    x = iris.data
+    y = iris.target
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True, stratify=y, random_state=0)
+
+    scaler = StandardScaler()
+    scaler.fit(x_train)
+    x_train = scaler.transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    model = KNN_Classification()
+    model.fit(x_train, y_train)
+    y_predict = model.predict(x_test)
+
+    print(classification_report(y_true=y_test, y_pred=y_predict))
+
+def test_regression():
+    diabetes = load_diabetes(as_frame=True)
+    x = diabetes.data
+    y = diabetes.target
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True, random_state=0)
+
+    scaler = StandardScaler()
+    scaler.fit(x_train)
+    x_train = scaler.transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    model = DecisionTreeRegressor()
+    model.fit(x_train, y_train)
+    y_predict = model.predict(x_test)
+
+    print("MAE: ", mean_absolute_error(y_true= y_test, y_pred=y_predict))
+    print("MSE: ", mean_squared_error(y_true= y_test, y_pred=y_predict))
+    print("R2:  ", r2_score(y_true= y_test, y_pred=y_predict))
+
+if __name__ == '__main__':
+    test_classification()
+#       precision    recall  f1-score   support
+#
+#            0       1.00      1.00      1.00        10
+#            1       0.91      1.00      0.95        10
+#            2       1.00      0.90      0.95        10
+#
+#     accuracy                           0.97        30
+#    macro avg       0.97      0.97      0.97        30
+# weighted avg       0.97      0.97      0.97        30
+
+    test_regression()
+# MAE:  66.0
+# MSE:  8053.4157303370785
+# R2:   -0.5705012345417619
